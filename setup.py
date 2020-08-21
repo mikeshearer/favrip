aa98b8d98907d14ba3bbd0a1c4efa7ad3a3336a1
Mike Shearer <mikesshearer@gmail.com>
2020-08-21 17:30:01 -0400

    8/21 - correcting setup

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ import re
 import setuptools
 
 _version_re = re.compile(r"__version__\s+=\s+(.*)")
-with open("ripper/__init__.py", "rb") as f:
+with open("favrip/__init__.py", "rb") as f:
 	match = _version_re.search(f.read().decode("utf-8"))
 	if match is None:
 		raise SystemExit(1)
 	version = str(ast.literal_eval(_match.group(1)))
 
 setuptools.setup(
-	name="ripper",
+	name="favrip",
 	version=version,
-	url="https://https://github.com/mikeshearer/ripper",
+	url="https://https://github.com/mikeshearer/favrip",
 	author="Mike Shearer",
 	author_email="MikeSShearer@gmail.com",
 	description="Scripts for asynchronously ripping elements from lists of websites",
